b6465eac7a5b5baa20e96d2d817a5280916a0a0b
Elkhan Isayev <is.elxan@gmail.com>
2025-01-15 11:20:16 +0400

    first init

---
+++ src/main/java/com/oms/msinventory/MsInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.oms.msinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oms/msinventory/MsInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oms.msinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
